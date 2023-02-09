abb7cfd286b4c0772582be8149458af1ccd10b34
RobotisDongun <hdu@robotis.com>
2023-02-09 13:31:29 +0900

    PI, CPI, HPI, H_off, H_down H_up ED_move, ED_down 등의 위치만을 파악하는 딥러닝 모델 학습을 위한 exp 파일 작성
    Signed-off-by: RobotisDongun <hdu@robotis.com>

---
+++ yolox/data/datasets/robotis_local_classes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+LOCATION_DETECT_CLASSES = [
+'ED_open',
+'ED_close',
+'ED_moving',
+'PI',
+'CPI',
+'H_up',
+'H_down',
+'H_off',
+]
